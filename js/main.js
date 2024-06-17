d4278873393052f5b794b1a3941bde53c84bc60c
Nirab Paudel <126984151+nirabpaudel@users.noreply.github.com>
2024-06-17 17:07:23 +0545

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
-navlink.addEventListener('click', () => {
-  document.getElementById('menuToggle').classList.add('closed');
+document.addEventListener("DOMContentLoaded", function() {
+  var toggle = document.getElementById('menuToggle');
+  var menuItems = document.querySelectorAll('#menu li a');
+
+  menuItems.forEach(function(item) {
+    item.addEventListener('click', function() {
+      toggle.checked = false; // Close the menu by unchecking the checkbox
+    });
+  });
 });
