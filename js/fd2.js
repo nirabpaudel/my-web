cbc083a3324b81845f95573056ea3d6314594c2b
Nirab Paudel <126984151+nirabpaudel@users.noreply.github.com>
2024-06-17 18:34:46 +0545

    Update fd2.js

--- js/fd2.js
+++ js/fd2.js
@@ -1,17 +1,15 @@
 document.addEventListener("DOMContentLoaded", function() {
   var toggle = document.getElementById('toggleMenu');
   var menuToggle = document.getElementById('menuToggle');
-  var originalTop = '43px'; // Adjust this to match original top position
-  var originalLeft = '17px'; // Adjust this to match original left position
+  var originalTop = '43px'; 
+  var originalLeft = '17px'; 
   
   toggle.addEventListener('change', function() {
     if (toggle.checked) {
-      // If checkbox is checked (menu is open), fix the menuToggle in place
       menuToggle.style.position = 'fixed';
-      menuToggle.style.top = '20px'; // Adjust these values as needed
-      menuToggle.style.left = '20px'; // Adjust these values as needed
+      menuToggle.style.top = '43px'; 
+      menuToggle.style.left = '17px'; 
     } else {
-      // If checkbox is unchecked (menu is closed), restore original position
       menuToggle.style.position = 'relative';
       menuToggle.style.top = originalTop;
       menuToggle.style.left = originalLeft;
