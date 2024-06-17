8ee6b864c286a9ce9ccf57b1a93906c99ced4b0f
Nirab Paudel <126984151+nirabpaudel@users.noreply.github.com>
2024-06-17 19:54:04 +0545

    Update fd3.js

--- js/fd3.js
+++ js/fd3.js
@@ -2,8 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
   var toggle = document.getElementById('toggleMenu');
   var menuLinks = document.querySelectorAll('#menu a');
   menuLinks.forEach(function(link) {
-    link.addEventListener('click', function() {
-      toggle.checked = false;
+    link.addEventListener('click', function(event) {
+      toggle.checked = !toggle.checked;
     });
   });
 });
